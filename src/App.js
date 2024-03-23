6e68222a122adf892ea5382a2a5a8aa9b9c2076f
ankit <ankit16web@gmail.com>
2024-03-23 11:19:48 +0530

    react push

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import './components/css/Header-Footer.css'
-import Header from "./components/Header";
+import Header from "./components//header/Header";
 import './components/css/style.css'
 import Banner from './components/index/Banner';
 import './components/css/global.css'
